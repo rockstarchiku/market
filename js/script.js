bcb11f6d332b66dc74cdde6f825a0221050e3e91
rockstarchiku <jngamau93@gmail.com>
2017-04-28 08:49:13 +0300

    Added variables for form

--- js/script.js
+++ js/script.js
@@ -1,24 +1,20 @@
-$(document).ready(function(){
-  $("button#hello").click(function(){
-    $("ul#user").prepend("<li>Hello!</li>");
-    $("ul#webpage").prepend("<li>Why hello there!</li>");
-    $("ul#user").children("li").first().click(function() {
-       alert('hi');
-     });
-     $("ul#webpage").children("li").first().click(function() {
-       alert('hi');
-     });
-    });
-  });
 
-  $("button#goodbye").click(function(){
-    $("ul#user").prepend("<li>Goodbye!</li>");
-    $("ul#webpage").prepend("<li>Goodbye, dear user!</li>");
-  });
+$(document).ready(function() {
 
-  $("button#stop").click(function(){
-    $("ul#user").prepend("<li>Stop copying me!</li>");
-    $("ul#webpage").prepend("<li>Pardon me. I meant no offense.</li>");
+  $(".user form").submit(function(event) {
 
+    var fullName = $("#fullname").val();
+    var address = $("#address").val();
+    var email = $("#email").val();
+
+
+
+    $(".fullname").text(fullName);
+    $(".address").text(address);
+    $(".email").text(email);
+
+
+
+  event.preventDefault();
   });
 });
